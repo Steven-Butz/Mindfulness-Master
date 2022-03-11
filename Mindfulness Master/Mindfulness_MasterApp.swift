a996da6f913062df57ce76c2ccd8621c95d559c2
Steven-Butz <sbutz2021@gmail.com>
2022-03-10 20:18:35 -0500

    Initial Commit

---
+++ Mindfulness Master/Mindfulness_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mindfulness_MasterApp.swift
+//  Mindfulness Master
+//
+//  Created by SButz on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Mindfulness_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
